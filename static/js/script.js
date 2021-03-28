637a52e7262ee8ba0062381f8a1fab926410ffe7
ShawnAhls <shawn.ahlstrom@icloud.com>
2021-03-28 12:09:30 +0000

    adding the email function to my portfolio where people can email me directly

--- static/js/script.js
+++ static/js/script.js
@@ -24,3 +24,25 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
 })
+
+
+(function() {
+    // https://dashboard.emailjs.com/admin/integration
+    emailjs.init('YOUR_USER_ID');
+})();
+
+window.onload = function() {
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        // generate a five digit number for the contact_number variable
+        this.contact_number.value = Math.random() * 100000 | 0;
+        // these IDs from the previous steps
+        emailjs.sendForm('contact_service', 'contact_form', this)
+            .then(function() {
+                console.log('SUCCESS!');
+            }, function(error) {
+                console.log('FAILED...', error);
+            });
+    });
+}
+    
